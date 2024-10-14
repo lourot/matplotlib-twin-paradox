67a4523868f6021f47c571b85b926d75de3bbb86
Aurélien Lourot <software@lourot.dev>
2024-10-14 14:42:16 +0200

    Styling

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import numpy as np
 def _main():
     fig, (axes_earth, axes_traveler) = plt.subplots(1, 2, figsize=(8, 8))
     _plot(axes_earth, "Earth")
-    _plot(axes_traveler, "Traveler")
+    _plot(axes_traveler, "Traveler", 1, ":")
     plt.show()
 
 
-def _plot(axes, title):
+def _plot(axes, title, linewidth=2, linestyle="-"):
+    earth_x = np.linspace(0, 0)
+    earth_y = np.linspace(0, 4)
     traveler_x_first_leg = np.linspace(0, 1)
     traveler_y_first_leg = 2 * traveler_x_first_leg
     traveler_x_second_leg = np.linspace(1, 0)
@@ -18,9 +20,31 @@ def _plot(axes, title):
     axes.set_title(title)
     axes.set_xlabel("x")
     axes.set_ylabel("t")
-    axes.plot(traveler_x_first_leg, traveler_y_first_leg, label="First leg")
-    axes.plot(traveler_x_second_leg, traveler_y_second_leg, label="Second leg")
-    axes.legend()
+    axes.plot(
+        earth_x,
+        earth_y,
+        label="Earth",
+        color="blue",
+        linewidth=linewidth,
+        linestyle=linestyle,
+    )
+    axes.plot(
+        traveler_x_first_leg,
+        traveler_y_first_leg,
+        label="First leg",
+        color="orange",
+        linewidth=linewidth,
+        linestyle=linestyle,
+    )
+    axes.plot(
+        traveler_x_second_leg,
+        traveler_y_second_leg,
+        label="Second leg",
+        color="orange",
+        linewidth=linewidth,
+        linestyle=linestyle,
+    )
+    # axes.legend()
 
 
 if __name__ == "__main__":
