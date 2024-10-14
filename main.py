584c7cdaedad2ce1d68eaf5a8a5a0afcc10510f4
Aurélien Lourot <software@lourot.dev>
2024-10-14 14:23:51 +0200

    Draw the second leg of the trip

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import numpy as np
 
 
 def _main():
-    x = np.linspace(0, 1)
-    y = 2 * x
+    traveler_x_first_leg = np.linspace(0, 1)
+    traveler_y_first_leg = 2 * traveler_x_first_leg
+    traveler_x_second_leg = np.linspace(1, 0)
+    traveler_y_second_leg = 2 + 2 * (1 - traveler_x_second_leg)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(4, 8))
     ax.set_title("Time diagram - Earth")
     ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.plot(x, y)
+    ax.set_ylabel("t")
+    ax.plot(traveler_x_first_leg, traveler_y_first_leg, label="First leg")
+    ax.plot(traveler_x_second_leg, traveler_y_second_leg, label="Second leg")
+    ax.legend()
     plt.show()
 
 
