8f30c04624ab7c50d9ec9f8ef239f4cb64845eed
Aurélien Lourot <software@lourot.dev>
2024-10-14 15:05:30 +0200

    Styling

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
+from typing import Any, Final
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def _main():
+def _main() -> None:
     fig, (axes_earth, axes_traveler) = plt.subplots(1, 2, figsize=(8, 8))
     _plot(axes_earth, "Earth")
     _plot(axes_traveler, "Traveler", 1, ":")
     plt.show()
 
 
-def _plot(axes, title, linewidth=2, linestyle="-"):
-    earth_x = np.linspace(0, 0)
-    earth_y = np.linspace(0, 4)
-    traveler_x_first_leg = np.linspace(0, 1)
-    traveler_y_first_leg = 2 * traveler_x_first_leg
-    traveler_x_second_leg = np.linspace(1, 0)
-    traveler_y_second_leg = 2 + 2 * (1 - traveler_x_second_leg)
-    markers_x = [
+def _plot(axes, title, linewidth=2, linestyle="-") -> None:
+    earth_x: Final[Any] = np.linspace(0, 0)
+    earth_y: Final[Any] = np.linspace(0, 4)
+    traveler_x_first_leg: Final[Any] = np.linspace(0, 1)
+    traveler_y_first_leg: Final[Any] = 2 * traveler_x_first_leg
+    traveler_x_second_leg: Final[Any] = np.linspace(1, 0)
+    traveler_y_second_leg: Final[Any] = 2 + 2 * (1 - traveler_x_second_leg)
+    markers_x: Final[Any] = [
         1,
     ]
-    markers_y = [
+    markers_y: Final[Any] = [
         2,
     ]
 
     axes.set_title(title)
-    axes.set_xlabel("x")
-    axes.set_ylabel("t")
+    label_color: Final[str] = "green"
+    label_fontsize: Final[int] = 14
+    axes.set_xlabel("x", color=label_color, fontsize=label_fontsize)
+    axes.set_ylabel("t", color=label_color, fontsize=label_fontsize)
     axes.plot(
         earth_x,
         earth_y,
@@ -51,6 +55,8 @@ def _plot(axes, title, linewidth=2, linestyle="-"):
         linestyle=linestyle,
     )
     axes.plot(markers_x, markers_y, "s", label="Other planet", color="red")  # s=square
+
+    axes.grid()
     # axes.legend()
 
 
